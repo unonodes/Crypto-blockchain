a505dc195ecb8a3e2978eec4e15432225f186a91
Rayan <87863722+unonodes@users.noreply.github.com>
2023-03-20 10:41:18 +0200

    Update SH-konstellation.sh

--- Konstellation/SH-konstellation.sh
+++ Konstellation/SH-konstellation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ~
 echo "$(source .bashrc)"
+echo "source: Bashrc"
 echo "$(source .bash_profile)"
+echo "source: Bash_profile"
 echo "$(source .profile)"
-echo "source: Source | Bash_profile | Profile"
+echo "source: Profile"
