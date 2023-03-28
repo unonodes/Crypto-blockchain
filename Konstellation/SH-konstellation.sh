df8b99bc1e610250191f7800a90197bfa9b46d98
Rayan <87863722+unonodes@users.noreply.github.com>
2023-03-28 20:52:29 +0300

    Delete SH-konstellation.sh

--- Konstellation/SH-konstellation.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-cd ~
-echo "$(source .bashrc) source: Bashrc"
-#echo "source: Bashrc"
-echo "$(source .bash_profile) source: Bash_profile"
-#echo "source: Bash_profile"
-echo "$(source .profile) source: Profile"
-echo "$(key_name_konstellation)"
-sleep 10
